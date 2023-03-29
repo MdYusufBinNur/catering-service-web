bbf5c7fd9cee3690bf49ecd6df839adaaaeb0e12
yusuf <binnur@tikweb.dk>
2023-03-29 16:28:06 +0600

    Test

--- components/Menus/MenuItems.vue
+++ components/Menus/MenuItems.vue
@@ -1,77 +1,98 @@
 <template>
-  <v-row>
-    <v-col :cols="bp.mdAndUp ? '4' : '12'"  v-for="(item, i) in items"
-           :key="i">
-      <v-card
-        rounded
-        :loading="loading"
-        class="mx-auto my-12"
-        max-width="374"
-      >
-        <template slot="progress">
-          <v-progress-linear
-            color="deep-purple"
-            height="10"
-            indeterminate
-          ></v-progress-linear>
-        </template>
-
-        <v-img
-          height="250"
-          :src="item.image"
-        ></v-img>
-
-        <v-card-title>{{ item.name }}</v-card-title>
-
-        <v-card-text>
-          <v-row
-            align="center"
-            class="mx-0"
-          >
-            <v-rating
-              :value="4.5"
-              color="amber"
-              dense
-              half-increments
-              readonly
-              size="14"
-            ></v-rating>
-
-            <div class="grey--text ms-4">
-              {{ item.rating }}
-            </div>
-          </v-row>
-
-          <div class="my-4 text-subtitle-1">
-            {{item.info}}
-          </div>
-
-        </v-card-text>
-
-        <v-divider class="mx-4"></v-divider>
-
-        <v-card-actions>
-          <v-card-text>
-            <v-chip-group
-              v-model="selection"
-              active-class="deep-purple accent-4 white--text"
-              column
+  <v-row align="" justify="center" style="justify-content: center">
+    <v-tabs
+      color="deep-purple accent-4"
+      class="text-capitalize"
+      style="justify-content: center"
+    >
+      <v-tab class="text-capitalize">Dinner</v-tab>
+      <v-tab class="text-capitalize">Lunch</v-tab>
+      <v-tab-item>
+        <v-row>
+          <v-col :cols="bp.mdAndUp ? '4' : '12'"  v-for="(item, i) in items"
+                 :key="i">
+            <v-card
+              rounded
+              :loading="loading"
+              class="mx-auto my-12"
+              max-width="374"
             >
-              <v-chip small class="deep-purple accent-4 white--text">{{item.availability}}</v-chip>
-              <v-chip small class="green darken-2 white--text">Price : {{item.price}}</v-chip>
+              <template slot="progress">
+                <v-progress-linear
+                  color="deep-purple"
+                  height="10"
+                  indeterminate
+                ></v-progress-linear>
+              </template>
 
-            </v-chip-group>
-          </v-card-text>
-        </v-card-actions>
+              <v-img
+                height="250"
+                :src="item.image"
+              ></v-img>
 
-      </v-card>
-    </v-col>
+              <v-card-title>{{ item.name }}</v-card-title>
 
+              <v-card-text>
+                <v-row
+                  align="center"
+                  class="mx-0"
+                >
+                  <v-rating
+                    :value="4.5"
+                    color="amber"
+                    dense
+                    half-increments
+                    readonly
+                    size="14"
+                  ></v-rating>
+
+                  <div class="grey--text ms-4">
+                    {{ item.rating }}
+                  </div>
+                </v-row>
+
+                <div class="my-4 text-subtitle-1">
+                  {{item.info}}
+                </div>
+
+              </v-card-text>
+
+              <v-divider class="mx-4"></v-divider>
+
+              <v-card-actions>
+                <v-card-text>
+                  <v-chip-group
+                    v-model="selection"
+                    active-class="deep-purple accent-4 white--text"
+                    column
+                  >
+                    <v-chip small class="deep-purple accent-4 white--text">{{item.availability}}</v-chip>
+                    <v-chip small class="info darken-4 white--text">Price : {{item.price}}</v-chip>
+
+                  </v-chip-group>
+                </v-card-text>
+              </v-card-actions>
+
+            </v-card>
+          </v-col>
+        </v-row>
+
+      </v-tab-item>
+
+    </v-tabs>
   </v-row>
 
 </template>
 
+
 <script>
+import special from 'static/special.jpg'
+import beef_2 from 'static/beef_2.jpg'
+import beef_bri from 'static/beef.jpg'
+import beef_hydra from 'static/beef-hydrabadi.jpg'
+import chik_bri from 'static/chicken-bri.jpg'
+import hydra_bri from 'static/hydrabadi.jpg'
+import mutton from 'static/mutton-biryani.jpeg'
 export default {
   name: "MenuItems",
   data: () => ({
@@ -82,7 +103,7 @@ export default {
         name : 'Sunday Special',
         rating: '4.5 (413)',
         availability: 'Sunday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+        image: special,
         info: 'Special Polaw, Chicken Roa ist, Beef Chilli, Chinese Veg, Jarda, Drinks',
         price: '$10'
       },
@@ -90,7 +111,7 @@ export default {
         name : 'Chicken Biriyani',
         rating: '4.5 (413)',
         availability: 'Monday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+        image: chik_bri,
         info: 'Chicken Biriyani, Kabab, Egg, Salad, Sweet, Drinks',
         price: '$10'
       },
@@ -98,7 +119,7 @@ export default {
         name : 'Mutton Biriyani',
         rating: '4.5 (413)',
         availability: 'Tuesday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+        image: hydra_bri,
         info: 'Mutton Biriyani, Kabab, Egg, Salad, Sweet, Drinks',
         price: '$10'
       },
@@ -106,15 +127,15 @@ export default {
         name : 'Beef Biriyani',
         rating: '4.5 (413)',
         availability: 'Wednesday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
-        info: 'Beef Biriyani, Kabab, Egg, Salad, Sweet, Drinks',
+        image: beef_2,
+        info: 'Beef Biriyani , Kabab , Egg , Salad, Sweet,  Drinks',
         price: '$10'
       },
       {
         name : 'Hydrabadi Chicken Biriyani',
         rating: '4.5 (413)',
         availability: 'Thursday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+        image: beef_bri,
         info: 'Hydrabadi Chicken Biriyani, Kabab, Egg, Salad, Sweet, Drinks',
         price: '$10'
       },
@@ -122,7 +143,7 @@ export default {
         name : 'Hydrabadi Mutton Biriyani',
         rating: '4.5 (413)',
         availability: 'Friday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+        image: mutton,
         info: 'Hydrabadi Mutton Biriyani, Kabab, Egg, Salad, Sweet, Drinks',
         price: '$10'
       },
@@ -130,7 +151,7 @@ export default {
         name : 'Hydrabadi Beef Biriyani',
         rating: '4.5 (413)',
         availability: 'Saturday',
-        image: 'https://cdn.vuetifyjs.com/images/cards/cooking.png',
+        image: beef_hydra,
         info: 'Hydrabadi Beef Biriyani, Kabab, Egg, Salad, Sweet, Drinks',
         price: '$10'
       },
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,61 @@
 <template>
   <v-app dark>
     <v-app-bar
-      :clipped-left="clipped"
       fixed
       app
       flat
     >
-      <v-list-item @click="$router.push('/')" :ripple="false" style="max-width: 280px" class="py-0">
-<!--        <v-img :src="logo" class="justify-center mb-4" :aspect-ratio="bp.mdAndUp ? 3 : 4" contain position="relative"-->
-<!--               align="center"></v-img>-->
-      </v-list-item>
-      <v-spacer />
-      <v-spacer />
-      <v-btn text :ripple="false" class="text-capitalize">
-        Menu
-      </v-btn>
-      <v-btn text :ripple="false" class="text-capitalize">
-        Privacy Policy
-      </v-btn>
-      <v-btn text :ripple="false" class="text-capitalize">
-        Terms & Conditions
-      </v-btn>
+      <v-row>
+        <v-col cols="12" md="4" sm="6" lg="4" >
+          <v-card-title v-show="bp.mdAndUp" class="pa-3">
+            <v-btn text class="text-capitalize"  :ripple="false" depressed>
+              The Great Spoon
+            </v-btn>
+          </v-card-title>
+        </v-col>
+        <v-col cols="12" md="4" sm="6" lg="4" >
+          <v-img :src="logo" :class="bp.mdAndUp ? 'justify-center mb-4' : 'justify-center mb-3'" :aspect-ratio="bp.mdAndUp ? 7 : 3" contain position="relative"
+                 align="center"></v-img>
+        </v-col>
+        <v-col cols="12" md="4" sm="6" lg="4" >
+          <v-btn text v-show="bp.mdAndUp" :ripple="false" class="text-capitalize">
+            Menu
+          </v-btn>
+          <v-btn text v-show="bp.mdAndUp"  :ripple="false" class="text-capitalize">
+            Privacy Policy
+          </v-btn>
+          <v-btn text v-show="bp.mdAndUp"  :ripple="false" class="text-capitalize">
+            Terms & Conditions
+          </v-btn>
+          <v-spacer v-if="bp.mdAndUp"/>
+          <v-icon @click.stop="drawer = !drawer" v-if="bp.smAndDown">mdi-sort-variant</v-icon>
+        </v-col>
+      </v-row>
+
     </v-app-bar>
+    <v-navigation-drawer
+      v-if="bp.smAndDown"
+      v-model="drawer"
+      light
+      app
+      right
+    >
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <v-container fluid>
         <Nuxt />
@@ -51,13 +85,13 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: 'Privacy Policy',
+          to: '/privacy-policy'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Terms & Condition',
+          to: '/terms-and-condition'
         }
       ],
       miniVariant: false,
